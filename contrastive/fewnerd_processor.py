f348d8e2310867d78d255ab76ae32c5e1a9ba342
Or Shachar <orsh>
2024-11-13 15:36:25 +0200

    querying from correct index

--- contrastive/fewnerd_processor.py
+++ contrastive/fewnerd_processor.py
@@ -1,17 +1,27 @@
+from random import shuffle
+from tqdm import tqdm
 import dataset_provider
 
 
 def yield_dataset(types, batch_size=50, instances_per_type=100):
     extract = lambda x: extract_entities_from_es_response(x)
+    anchors, good_bathches, bad_batches = [], [], []
 
     for result in dataset_provider.random_results_per_fine_type(types, instances_per_type):
         result_type = result["_source"]["fine_type"]
-        good_batch = dataset_provider.get_by_fine_grained_type_fewnerd_v3(result_type,  randomize=True, batch_size=batch_size)
+        good_batch = dataset_provider.get_by_fine_grained_type_fewnerd_v4(result_type,  randomize=True, batch_size=batch_size)
         other_types = [t for t in types if t != result_type]
-        bad_batch = dataset_provider.get_by_fine_grained_type_fewnerd_v3(other_types, randomize=True, batch_size=batch_size)
+        bad_batch = dataset_provider.get_by_fine_grained_type_fewnerd_v4(other_types, randomize=True, batch_size=batch_size)
+        anchors.append(result["_source"])
+        good_bathches.append(extract(good_batch))
+        bad_batches.append(extract(bad_batch))
 
-        yield result["_source"], extract(good_batch), extract(bad_batch)
+    order_of_results = list(range(len(anchors)))
+    shuffle(order_of_results)
+    for i in tqdm(order_of_results):
+        yield anchors[i], good_bathches[i], bad_batches[i]
 
+    del anchors, good_bathches, bad_batches
 
 def yield_train_dataset(batch_size=50, instances_per_type=100):
     types = ['education', 'airport', 'restaurant', 'sportsleague', 'disease', 'hospital', 'painting', 'other',

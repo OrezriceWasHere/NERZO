f1da09cf0a7d439d8cbaf58f1ba081859da75b19
Or Shachar <orsh>
2025-07-15 16:24:19 +0300

    supporting nertreive

--- evaluate_with_extraction/extracting/extracting_entities_nertrieve.py
+++ evaluate_with_extraction/extracting/extracting_entities_nertrieve.py
@@ -26,12 +26,13 @@ from base_extractor import (
 )
 
 import clearml_poc
+from evaluate_with_extraction.extracting.base_extractor import align_to_original
 
 # --------------------------------------------------------------------
 # Settings
 # --------------------------------------------------------------------
 BATCH_SIZE = 100  # GPU batch
-MAX_NEW = 4096  # generation cut-off
+MAX_NEW = 4096 * 4  # generation cut-off
 
 REPO = "CascadeNER/models_for_CascadeNER"
 SUBF = "extractor"
@@ -81,11 +82,8 @@ def parse_base_dataset(corpus_file, entities_file) -> Dict[str, Dict]:
                             continue
                         start_word = min(location)
                         end_word = max(location)
-                        start_index = (
-                            len(" ".join(split[0:start_word])) + 1
-                            if start_word > 0
-                            else 0
-                        )
+                        space_offset = 1 if start_word > 0 else 0
+                        start_index = len(" ".join(split[0:start_word])) + space_offset
                         end_index = len(" ".join(split[0 : end_word + 1]))
                         gold.append(
                             {
@@ -147,7 +145,7 @@ if __name__ == "__main__":
         gold_lists=gold_lists,
         gold_sets=gold_sets,
         ids=ids,
-        extract_fn=extract_entities_with_positions,
+        extract_fn=align_to_original,
         batch_size=BATCH_SIZE,
         max_new_tokens=MAX_NEW,
         dataset_project="nertrieve_pipeline",
